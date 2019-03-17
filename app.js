0aa48023063b2f6a59470a03a7c3319bed1655c4
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-17 14:18:56 +0000

    Added remove tasks from local storage function

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ function addTask(e) {
 }
 
 
-// Store Task
+// Store Task in local storage
 
 function storeTaskInLocalStorage(task) {
     let tasks;
@@ -124,9 +124,33 @@ function removeTask(e) {
     if (e.target.parentElement.classList.contains('delete-item')) {
         if (confirm('Are you sure you want to delete the task?')) {
             e.target.parentElement.parentElement.remove();
+            
+            // Remove task from local storage
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
 }
+
+// Remove Task from local storage
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    
+    tasks.forEach(function(task, index) {
+        if (taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    });
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
 
 // Clear Tasks
 
